bb3628eb18ad3e2d3f962b0ab1a4914314fb57c8
GowthamSKdev <gowthams2647@gmail.com>
2025-09-29 21:46:25 +0530

    fixed

--- src/components/WalkerAgency.jsx
+++ src/components/WalkerAgency.jsx
@@ -2,7 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/SplitText";
 
-import Team_image from "../assets/Images/team.JPG";
+import Team_image from "../assets/Images/team.jpg";
 
 export default function WalkerAgency() {
   useGSAP(() => {
